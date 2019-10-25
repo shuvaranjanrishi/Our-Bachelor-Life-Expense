37d845e25d15ab2b9e368d2fb9ecf4c8a059ad27
Shonpnil <shopnershopnil44@gmail.com>
2019-10-25 15:07:43 +0600

    consumers fragment added

---
+++ app/src/main/java/com/example/asus/bechelorlifeexpense/fragment/ConsumerFragment.java
@@ -0,0 +1,26 @@
+package com.example.asus.bechelorlifeexpense.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.asus.bechelorlifeexpense.R;
+
+public class ConsumerFragment extends Fragment {
+
+    public ConsumerFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_consumer, container, false);
+        return view;
+    }
+}
