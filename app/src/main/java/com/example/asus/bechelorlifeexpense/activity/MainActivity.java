0de62841c0d5ec8f307db1e8291e3a3412bad809
Shonpnil <shopnershopnil44@gmail.com>
2019-10-27 00:01:53 +0600

    options menu created and backup,restore,clear all data action performed

--- app/src/main/java/com/example/asus/bechelorlifeexpense/activity/MainActivity.java
+++ app/src/main/java/com/example/asus/bechelorlifeexpense/activity/MainActivity.java
@@ -1,22 +1,38 @@
 package com.example.asus.bechelorlifeexpense.activity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.asus.bechelorlifeexpense.R;
+import com.example.asus.bechelorlifeexpense.database.MyDBHelper;
 import com.example.asus.bechelorlifeexpense.fragment.ConsumerFragment;
 import com.example.asus.bechelorlifeexpense.fragment.DashBoardFragment;
 import com.example.asus.bechelorlifeexpense.fragment.ExpensesFragment;
+import com.example.asus.bechelorlifeexpense.local_backup.LocalBackup;
+
+import java.io.File;
+
+import static com.example.asus.bechelorlifeexpense.fragment.ExpensesFragment.myDBHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int mark = 0;
+    private boolean selectedFragment = true;
     private BottomNavigationView bottomNavigationView;
+    private LocalBackup localBackup;
+
+    public static final int REQUEST_CODE_PERMISSIONS = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,28 +52,26 @@ public class MainActivity extends AppCompatActivity {
                 switch (menuItem.getItemId()){
                     case R.id.dashBoardNavItemId:
 
-                        //if(mark == 1){
+                        if(selectedFragment == false){
                             replaceFragment(new DashBoardFragment());
-                           // mark--;
-                        //}
+                            selectedFragment = true;
+                        }
                         setTitle("Dash Board");
                         return true;
 
                     case R.id.consumerNavItemId:
 
-                        //if(mark == 0){
+                        if(selectedFragment){
                             replaceFragment(new ConsumerFragment());
-                         //   mark++;
-                        //}
+                           selectedFragment = false;
+                        }
                         setTitle("Consumers");
                         return true;
 
                     case R.id.expensesNavItemId:
 
-                        //if(mark == 0){
                             replaceFragment(new ExpensesFragment());
-                        //    mark++;
-                     //   }
+
                         setTitle("Expenses");
                         return true;
                 }
@@ -74,8 +88,70 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
+    //menu item action
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        MyDBHelper myDBHelper = new MyDBHelper(getApplicationContext());
+
+        switch (item.getItemId()) {
+            case R.id.createBackupItemId:
+                String outFileName = Environment.getExternalStorageDirectory() + File.separator + getResources().getString(R.string.app_name) + File.separator;
+                localBackup.performBackup(myDBHelper, outFileName);
+                break;
+
+            case R.id.importDataItemId:
+                localBackup.performRestore(myDBHelper);
+                break;
+
+            case R.id.clearAllDataItemId:
+
+                showAlertDialog();
+
+                break;
+
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Are you sure to clear all data !");
+
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                SQLiteDatabase sqLiteDatabase = myDBHelper.getWritableDatabase();
+                myDBHelper.onUpgrade(sqLiteDatabase,MyDBHelper.getDatabaseVersion(),MyDBHelper.getDatabaseVersion());
+                Toast.makeText(MainActivity.this, "All data cleared\nplease restart your app", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        Dialog dialog = builder.create();
+        dialog.show();
+
+    }
+
     //initialize all component
     private void init() {
         bottomNavigationView = findViewById(R.id.bottomNavigationViewId);
+        localBackup = new LocalBackup(this);
     }
 }
